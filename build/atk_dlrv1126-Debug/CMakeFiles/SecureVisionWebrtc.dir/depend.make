3decf92b949e87f569e5cb3246e9ef69666e370c
zhenglongbin <zhenglongbin1@gmail.com>
2025-07-05 16:51:07 +0800

    Remove build/ and user files from Git history

--- build/atk_dlrv1126-Debug/CMakeFiles/SecureVisionWebrtc.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for SecureVisionWebrtc.
-# This may be replaced when dependencies are built.
--- build/atk_dlrv1126-Debug/CMakeFiles/SecureVision_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/SecureVision_autogen"
-  "SecureVision_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/SecureVision_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
